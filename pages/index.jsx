db947a7c9352aa83b097b20d9ae684c494b0b94a
Ålexander <yurchenko@post.com>
2022-08-15 17:50:19 +0200

    Fixed the mobile view for very very VERY small phones

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 // CSS:
 import styles from "../styles/Home.module.css";
+// Next components:
+import Link from "next/link";
 
 const Home = () => {
   return (
@@ -19,6 +21,19 @@ const Home = () => {
         <div className={styles.decoRight1}></div>
         <div className={styles.decoRight2}></div>
       </section>
+      <aside className={styles.aside}>
+        <h3>
+          Load the latest tweets about the{" "}
+          <Link href="/characters">
+            <a className={styles.a}>characters</a>
+          </Link>{" "}
+          of Babylon 5 TV series
+        </h3>
+        <div className={styles.decoLeft1}></div>
+        <div className={styles.decoLeft2}></div>
+        <div className={styles.decoRight1}></div>
+        <div className={styles.decoRight2}></div>
+      </aside>
     </div>
   );
 };
