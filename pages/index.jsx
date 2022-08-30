b253c95354a458e8475a02e264bbf6f1859b0b67
Ålexander <yurchenko@post.com>
2022-08-30 12:39:03 +0200

    Added meta tags for SEO

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,6 +9,16 @@ const Home = () => {
     <div className={styles.wrapper}>
       <Head>
         <title>Babylon 5 tweets lookup</title>
+        <meta property="og:url" content="bylon5lookup.vercel.app/" />
+        <meta property="og:title" content="Babylon 5 tweets lookup" />
+        <meta
+          property="og:description"
+          content="Check up the latest tweets about the characters of Babylon 5 TV series!"
+        />
+        <meta
+          property="og:image"
+          content="http://github.com/rockysfairat/b5lookup/blob/main/public/B5screenshot.png"
+        />
       </Head>
       <section className={styles.section}>
         <h2>
