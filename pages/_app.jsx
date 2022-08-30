e742a38ec8ddec6a2fba4684191f6c1f9d80c29f
Ålexander <yurchenko@post.com>
2022-08-30 13:17:44 +0200

    Modified the meta tags

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -12,15 +12,15 @@ const App = ({ Component, pageProps }) => {
         <meta property="og:title" content="Babylon 5 tweets lookup" />
         <meta
           property="og:description"
-          content="Check up the latest tweets about the characters of Babylon 5 TV series!"
+          content="Check up the latest tweets about the characters of Babylon 5 TV series! A webpage that shows the latest tweets about Babylon 5 tv series or/and characters of this show."
         />
         <meta
           property="og:image"
           content="http://raw.githubusercontent.com/rockysfairat/b5lookup/main/public/B5screenshot.jpg"
         />
         <meta property="og:image:width" content="1200" />
-
         <meta property="og:image:height" content="630" />
+        <meta property="author" content="Ålexander Yurchenko" />
       </Head>
       <Component {...pageProps} />
     </Layout>
