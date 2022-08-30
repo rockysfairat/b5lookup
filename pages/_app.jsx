b371d719a7a448d89f48e4f9d66cb44fb79da4d6
Ålexander <yurchenko@post.com>
2022-08-30 12:53:15 +0200

    Modified _app.js by adding meta tags

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,24 @@
 import Layout from "../components/Layout";
+import Head from "next/head";
 import "../styles/globals.css";
 
 const App = ({ Component, pageProps }) => {
   return (
     <Layout>
+      <Head>
+        <title>Babylon 5 tweets lookup</title>
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:url" content="bylon5lookup.vercel.app/" />
+        <meta property="og:title" content="Babylon 5 tweets lookup" />
+        <meta
+          property="og:description"
+          content="Check up the latest tweets about the characters of Babylon 5 TV series!"
+        />
+        <meta
+          property="og:image"
+          content="http://github.com/rockysfairat/b5lookup/blob/main/public/B5screenshot.jpg"
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
